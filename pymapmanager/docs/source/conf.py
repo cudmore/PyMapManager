4c4dcb633781b85eb43a79ad13cfab055f29c789
cudmore <robert.cudmore@gmail.com>
2018-01-12 00:00:19 -0500

    Xxx
    Former-commit-id: 590c4a73b9744ec0a0976f93535bf296b62f8264

--- pymapmanager/docs/source/conf.py
+++ pymapmanager/docs/source/conf.py
@@ -21,6 +21,14 @@ import os, sys
 
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
+"""
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+"""
+
 if on_rtd:
     #from unittest.mock import MagicMock
     from mock import MagicMock
