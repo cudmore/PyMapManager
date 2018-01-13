82cf543abca343a76193e0599c7be21b0d428702
cudmore <robert.cudmore@gmail.com>
2018-01-13 16:55:50 -0500

    desktop
    Former-commit-id: a9752760928791caba87a0fd7da04930dd72098c

--- pymapmanager/version.py
+++ pymapmanager/version.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 The version of map manager.
 
@@ -9,6 +8,3 @@ Example::
 """
 __version__ = '0.0.3'
 
-=======
-__version__ = '0.1.3'
->>>>>>> 1fafab487d3f9fa7524c831a1474814132f4a645
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 '''
 ## Pushing to PyPi
@@ -16,25 +16,6 @@ https://pypi.python.org/pypi/pymapmanager
 
 2. [old] Update version in `PyMapManager/setup.py`
 
-<<<<<<< HEAD
-'''
-## Pushing to PyPi
-
-https://pypi.python.org/pypi/pymapmanager
-
-1. Make sure there is a `~/.pypirc` file
-
-	[distutils]
-	index-servers=pypi
-
-	[pypi]
-	username=your_username
-	password=your_password
-
-2. [old] Update version in `PyMapManager/setup.py`
-
-=======
->>>>>>> 1fafab487d3f9fa7524c831a1474814132f4a645
       version='0.1.1',
 
 3. Makes .tar.gz in `dist/`
@@ -52,30 +33,19 @@ https://pypi.python.org/pypi/pymapmanager
 exec (open('pymapmanager/version.py').read())
 
 setup(
-<<<<<<< HEAD
     name='pymapmanager',
-=======
-    name='PyMapManager',
->>>>>>> 1fafab487d3f9fa7524c831a1474814132f4a645
     version=__version__,
     description='Load, analyze, and visualize Map Manager files',
     url='http://github.com/cudmore/PyMapManager',
     author='Robert H Cudmore',
     author_email='robert.cudmore@gmail.com',
     license='MIT',
-<<<<<<< HEAD
     packages = find_packages(),
     #packages = find_packages(exclude=['version']),
     #packages=[
     #    'pymapmanager',
     #    'pymapmanager.mmio'
     #],
-=======
-    packages=[
-        'pymapmanager',
-        'pymapmanager.mmio'
-    ],
->>>>>>> 1fafab487d3f9fa7524c831a1474814132f4a645
     install_requires=[
         "numpy>=1.14.0",
         "pandas>=0.22.0",
